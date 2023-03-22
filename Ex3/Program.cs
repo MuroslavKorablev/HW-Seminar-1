cfa8410164e859ff63913e760e60ea51c35c4dd9
Kleo <mmttdd16@gmail.com>
2023-03-22 19:15:25 +0300

    Finished Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine(number +" Является натуральным числом");
+}
+else
+{
+    Console.WriteLine(number + " Не является натуральным");
+}
