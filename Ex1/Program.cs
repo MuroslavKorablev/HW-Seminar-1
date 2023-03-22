f9bf50f069a46773effaa7a3f3cf20f7e88576d6
Kleo <mmttdd16@gmail.com>
2023-03-22 07:27:23 +0300

    7:28

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//int a = 5;
+//int b = 7;
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    int max = a;
+    //System.Console.WriteLine(max);
+    System.Console.WriteLine($"{max} > {b} = {max = a}");
+}
+else
+{
+    int max = b;
+    //int
+   //System.Console.WriteLine(max);
+   System.Console.WriteLine($"{max} > {a} = {max = b}");
+}
+
+//    System.Console.WriteLine($"{a} > {b} = {a > b}");
