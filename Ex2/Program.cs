d7d8a78b2592d33ea0d2e9ee72b2f91197097171
Kleo <mmttdd16@gmail.com>
2023-03-22 16:45:41 +0300

    Finished Ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0;
+
+System.Console.WriteLine("ведите первое число");
+int firstN = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ведите второе число");
+int secondN = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ведите третье число");
+int thirdN = Convert.ToInt32(Console.ReadLine());
+
+if(firstN > max)
+{
+    max = firstN;
+
+}
+if (secondN > max)
+{
+    max = secondN;
+}
+
+if (thirdN > max)
+{
+    max = thirdN;
+}
+
+System.Console.WriteLine("max = " + max);
