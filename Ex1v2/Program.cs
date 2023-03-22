323e46f5adccfe431af43b4ec32e7b5061119bb1
Kleo <mmttdd16@gmail.com>
2023-03-22 16:04:23 +0300

    Finished Ex1v2

--- Ex1v2/Program.cs
+++ Ex1v2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int min = 0;
+int max = 0;
+
+System.Console.WriteLine("Ведите первое число");
+int firstN = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Ведите второе число");
+int secondN = Convert.ToInt32(Console.ReadLine());
+
+if(firstN > secondN)
+{
+    min = secondN;
+    max = firstN;
+}
+else
+{
+  min = firstN;
+  max = secondN;  
+}
+//System.Console.WriteLine($"{max} > {min} = {max = }"); не правильно
+System.Console.WriteLine("max = " + max + " и " + "min = " + min);
