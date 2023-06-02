e7595c6ef0b32e233b0fb741ef533b82f5720fbc
Kleo <mmttdd16@gmail.com>
2023-06-02 22:23:49 +0300

    Finished Ex5

---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int remDiv = number % 2;
+
+// if(remDiv == 0)'
+if (number % 2 == 0)
+
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+     Console.WriteLine(" Число нечетное");
+}
---
+++ Ex5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int thirdDigit = FindThirdDigit(number);
+
+        if (thirdDigit != -1)
+        {
+            Console.WriteLine($"Третья цифра слева: {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Такой цифры нет");
+        }
+    }
+
+    static int FindThirdDigit(int number)
+    {
+        int digitCount = CountDigits(number);
+
+        if (digitCount < 3)
+        {
+            return -1; // Число имеет меньше трех цифр
+        }
+
+        int divisor = (int)Math.Pow(10, digitCount - 3);
+        int truncatedNumber = number / divisor;
+
+        return truncatedNumber % 10;
+    }
+
+    static int CountDigits(int number)
+    {
+        int count = 0;
+        while (number != 0)
+        {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
+}
+
+
+
